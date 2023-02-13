1bcb66ac1a307b7b660df65dfcbc9343c2e4b2e1
Josen3D <antonio.duran.90475@gmail.com>
2023-02-13 12:37:27 -0600

    employee update response added

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -71,7 +71,10 @@ export const updateEmployee = async (req, res) => {
         message: "Employee Not Found",
       });
 
-    res.json("Employee Updated");
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
+
+    res.json(rows);
+    
   } catch (error) {
     return res.status(500).json({
       message: "Something goes wrong",
